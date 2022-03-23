35cbe44bbac75a87b1b766e43d0c5211dc479d73
davidevaliante <dav.valiante@gmail.com>
2022-03-23 11:09:24 +0100

    header compliance

--- pages/b/[bonuses]/index.tsx
+++ pages/b/[bonuses]/index.tsx
@@ -1,104 +1,123 @@
 import React, { FunctionComponent, useEffect, useContext } from 'react'
-import AquaClient from '../../../graphql/aquaClient';
-import { BONUSES_BY_NAME, STREAMER_BY_ID } from '../../../graphql/queries/bonus';
-import { Bonus } from '../../../graphql/schema';
-import styled from 'styled-components';
-import { tablet } from '../../../components/Responsive/Breakpoints';
-import { initializeAnalytics } from '../../../analytics/base';
-import { cookieContext } from '../../../context/CookieContext';
-import CookieDisclaimer from '../../../components/CookieDisclaimer/CookieDisclaimer';
-import VideoDiscalimer from '../../../components/VideoDisclaimer/VideoDisclaimer';
-import BonusStripe from '../../../components/BonusStripe/BonusStripe';
-import { Streamer, StreamerBonus } from '../../../models/streamer';
-import {configuration} from '../../../configuration'
-import axios from 'axios';
-import  Router  from 'next/router';
-import lowerCase  from 'lodash/lowerCase'
-import { useState } from 'react';
-import { CircularProgress } from '@material-ui/core';
-import FullPageLoader from '../../../components/FullPageLoader';
-import Wrapper from '../../../components/Layouts/Wrapper';
-import Container from '../../../components/Layouts/Container';
+import AquaClient from '../../../graphql/aquaClient'
+import { BONUSES_BY_NAME, STREAMER_BY_ID } from '../../../graphql/queries/bonus'
+import { Bonus } from '../../../graphql/schema'
+import styled from 'styled-components'
+import { tablet } from '../../../components/Responsive/Breakpoints'
+import { initializeAnalytics } from '../../../analytics/base'
+import { cookieContext } from '../../../context/CookieContext'
+import CookieDisclaimer from '../../../components/CookieDisclaimer/CookieDisclaimer'
+import VideoDiscalimer from '../../../components/VideoDisclaimer/VideoDisclaimer'
+import BonusStripe from '../../../components/BonusStripe/BonusStripe'
+import { Streamer, StreamerBonus } from '../../../models/streamer'
+import { configuration } from '../../../configuration'
+import axios from 'axios'
+import Router from 'next/router'
+import lowerCase from 'lodash/lowerCase'
+import { useState } from 'react'
+import { CircularProgress } from '@material-ui/core'
+import FullPageLoader from '../../../components/FullPageLoader'
+import Wrapper from '../../../components/Layouts/Wrapper'
+import Container from '../../../components/Layouts/Container'
 
 interface Props {
-    streamerData : Streamer
-    bonusToShow : number[]
+	streamerData: Streamer
+	bonusToShow: number[]
 }
 
 const Compare: FunctionComponent<Props> = ({ streamerData, bonusToShow }) => {
+	const [country, setCountry] = useState<string | undefined>(undefined)
+	const [bonuses, setBonuses] = useState<StreamerBonus[] | undefined>(
+		undefined
+	)
 
+	useEffect(() => {
+		geoLocate()
+	}, [])
 
-    const [country, setCountry] = useState<string | undefined>(undefined)
-    const [bonuses, setBonuses] = useState<StreamerBonus[] | undefined>(undefined)
+	const geoLocate = async () => {
+		getBonusByName()
+		setCountry('it')
+	}
 
-  
-    useEffect(() => {
-        geoLocate()
-    }, [])
+	const getBonusByName = () => {
+		const streamerBonuses = streamerData.bonuses
+		const placeholder: StreamerBonus[] = []
 
-    const geoLocate = async () => {
-        getBonusByName()
-        setCountry('it')
-    }
+		console.log(bonusToShow)
 
-    const getBonusByName = () => {
-        const streamerBonuses = streamerData.bonuses
-        const placeholder : StreamerBonus[] = []
+		bonusToShow.forEach(bonusCode => {
+			const b = streamerBonuses.find(b => b.id === bonusCode)
+			if (b) placeholder.push(b)
+		})
 
-        console.log(bonusToShow)
+		setBonuses(placeholder)
+		console.log(placeholder, 'bonus to show')
+	}
 
-        bonusToShow.forEach((bonusCode) =>{
-            const b = streamerBonuses.find(b => b.id === bonusCode)
-            if(b) placeholder.push(b)
-        })
+	if (!country) return <FullPageLoader />
+	return (
+		<Wrapper>
+			<Container>
+				<div className='top-bar' style={{ cursor: 'pointer' }}>
+					<img className='logo' src='/icons/app_icon.png' />
+				</div>
 
-        setBonuses(placeholder)
-        console.log(placeholder, 'bonus to show')
-    }
-    
-    if(!country) return <FullPageLoader />
-    return (
-        <Wrapper>
-            <Container>
-                <div className='top-bar' style={{cursor : 'pointer'}} >
-                    <img className='logo' src='/icons/app_icon.png' />
-                </div>
+				<h1>Comparazione offerte siti legali in Italia:</h1>
 
-                <h1>Migliori casinò legali dove trovare questi giochi:</h1>
+				{bonuses &&
+					bonuses.length > 2 &&
+					bonuses.map((bonus: StreamerBonus) => (
+						<BonusStripe
+							key={`${bonus.name}`}
+							bonus={bonus}
+							countryCode={country}
+						/>
+					))}
 
-                {bonuses && bonuses.length > 2 && bonuses.map((bonus : StreamerBonus) => <BonusStripe key={`${bonus.name}`} bonus={bonus} countryCode={country} />)}
+				{bonuses &&
+					bonuses.length <= 2 &&
+					streamerData.bonuses.map((bonus: StreamerBonus) => (
+						<BonusStripe
+							key={`${bonus.name}`}
+							bonus={bonus}
+							countryCode={country}
+						/>
+					))}
 
-                {bonuses && bonuses.length <= 2 && streamerData.bonuses.map((bonus : StreamerBonus) => <BonusStripe key={`${bonus.name}`} bonus={bonus} countryCode={country} />)}
-
-                <div style={{ padding: '1rem' }}>
-                    <VideoDiscalimer />
-                </div>
-                <div className='bottom'>
-                    <p style={{textAlign : 'center'}}>This service is provided by <a href='https://www.topaffiliation.com'>Top Affiliation</a></p>
-                </div>
-            </Container>
-        </Wrapper>
-    )
-
+				<div style={{ padding: '1rem' }}>
+					<VideoDiscalimer />
+				</div>
+				<div className='bottom'>
+					<p style={{ textAlign: 'center' }}>
+						This service is provided by{' '}
+						<a href='https://www.topaffiliation.com'>
+							Top Affiliation
+						</a>
+					</p>
+				</div>
+			</Container>
+		</Wrapper>
+	)
 }
 
-
 export async function getServerSideProps({ query }) {
+	const pickedBonus = query.bonuses
 
-    const pickedBonus = query.bonuses
+	const aquaClient = new AquaClient()
 
-    const aquaClient = new AquaClient()
+	const bonusToShow = pickedBonus.split('-').map(b => parseInt(b))
 
-    const bonusToShow = pickedBonus.split('-').map(b => parseInt(b))
+	const streamer = await axios.get(
+		`${configuration.api}/streamers/${configuration.streamerId}`
+	)
 
-    const streamer = await axios.get(`${configuration.api}/streamers/${configuration.streamerId}`)
-   
-    return {
-        props: {
-            streamerData : streamer.data as Streamer,
-            bonusToShow : bonusToShow,
-        }
-    }
+	return {
+		props: {
+			streamerData: streamer.data as Streamer,
+			bonusToShow: bonusToShow,
+		},
+	}
 }
 
 export default Compare
