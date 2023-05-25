46ab02f1811f68d728186550bf8af1336cfd63a3
davidevaliante <dav.valiante@gmail.com>
2023-05-25 22:47:16 +0200

    removed page

--- pages/b/[bonuses]/index.tsx
+++ pages/b/[bonuses]/index.tsx
@@ -1,176 +1,56 @@
-import React, { FunctionComponent, useEffect, useContext } from 'react'
-import AquaClient from '../../../graphql/aquaClient'
-import { BONUSES_BY_NAME, STREAMER_BY_ID } from '../../../graphql/queries/bonus'
-import { Bonus } from '../../../graphql/schema'
-import styled from 'styled-components'
-import { tablet } from '../../../components/Responsive/Breakpoints'
-import { initializeAnalytics } from '../../../analytics/base'
-import { cookieContext } from '../../../context/CookieContext'
-import CookieDisclaimer from '../../../components/CookieDisclaimer/CookieDisclaimer'
-import VideoDiscalimer from '../../../components/VideoDisclaimer/VideoDisclaimer'
-import BonusStripe from '../../../components/BonusStripe/BonusStripe'
-import { Streamer, StreamerBonus } from '../../../models/streamer'
-import { configuration } from '../../../configuration'
-import axios from 'axios'
-import Router from 'next/router'
-import lowerCase from 'lodash/lowerCase'
-import { useState } from 'react'
-import { CircularProgress } from '@material-ui/core'
-import FullPageLoader from '../../../components/FullPageLoader'
-import Wrapper from '../../../components/Layouts/Wrapper'
-import Container from '../../../components/Layouts/Container'
+import React, { FunctionComponent, useEffect, useState } from "react";
+import axios from "axios";
+import { configuration } from "../configuration";
+import AquaClient from "../graphql/aquaClient";
+import { Streamer, StreamerBonus } from "../models/streamer";
+import BonusStripe from "../components/BonusStripe/BonusStripe";
+import VideoDiscalimer from "../components/VideoDisclaimer/VideoDisclaimer";
+import FullPageLoader from "../components/FullPageLoader";
+import lowerCase from "lodash/lowerCase";
+import Wrapper from "../../../components/Layouts/Wrapper";
+import Container from "../../../components/Layouts/Container";
 
 interface Props {
-	streamerData: Streamer
-	bonusToShow: number[]
+  streamerData: Streamer;
 }
 
-const Compare: FunctionComponent<Props> = ({ streamerData, bonusToShow }) => {
-	const [country, setCountry] = useState<string | undefined>(undefined)
+const index: FunctionComponent<Props> = ({ streamerData }) => {
+  return (
+    <Wrapper>
+      <Container>
+        <div className="top-bar">
+          <img className="logo" src="/icons/app_icon.png" />
+        </div>
+        <div>
+          <p style={{ margin: "3rem", marginTop: "4rem" }}>
+            Nel 2015 Paolo e Mimmo hanno creato IL COBRA per condividere con una
+            community crescente la loro passione principale: il calcio e lo
+            sport in generale. Poi è arrivato Anso...
+          </p>
 
-	useEffect(() => {
-		if (country !== '') getBonusByName()
-	}, [country])
-	const [bonuses, setBonuses] = useState<StreamerBonus[] | undefined>(
-		undefined
-	)
-
-	useEffect(() => {
-		geoLocate()
-	}, [])
-
-	const geoLocate = async () => {
-		const userCountryRequest = await axios.get(configuration.geoApi)
-		const countryCode = lowerCase(userCountryRequest.data.country_code2)
-		setCountry(countryCode)
-	}
-
-	const getBonusByName = () => {
-		const streamerBonuses = streamerData.bonuses
-		const placeholder: StreamerBonus[] = []
-
-		console.log(bonusToShow)
-
-		bonusToShow.forEach(bonusCode => {
-			const b = streamerBonuses.find(b => b.id === bonusCode)
-			if (b) placeholder.push(b)
-		})
-
-		setBonuses(placeholder)
-		console.log(placeholder, 'bonus to show')
-	}
-
-	if (!country) return <FullPageLoader />
-	if (country !== 'it' && country !== 'mt')
-		// if (country === 'it')
-		return (
-			<Wrapper>
-				<Container>
-					<div className='top-bar' style={{ cursor: 'pointer' }}>
-						<img className='logo' src='/icons/app_icon.png' />
-					</div>
-
-					<div>
-						<h1 style={{ margin: '1rem 0rem' }}>
-							Watch more YouTube videos :
-						</h1>
-						<div className='rwd-video'>
-							<iframe
-								src={`https://www.youtube.com/embed/290b2Pmj0ZI`}
-								allowFullScreen
-								frameBorder='0'
-								height='315'
-								width='560'
-							/>
-						</div>
-
-						<div className='rwd-video'>
-							<iframe
-								src={`https://www.youtube.com/embed/NXZsfSlZvL0`}
-								allowFullScreen
-								frameBorder='0'
-								height='315'
-								width='560'
-							/>
-						</div>
-
-						<div className='rwd-video'>
-							<iframe
-								src={`https://www.youtube.com/embed/X4wJDs8A5lQ`}
-								allowFullScreen
-								frameBorder='0'
-								height='315'
-								width='560'
-							/>
-						</div>
-					</div>
-				</Container>
-			</Wrapper>
-		)
-	return (
-		<Wrapper>
-			<Container>
-				<div className='top-bar' style={{ cursor: 'pointer' }}>
-					<img className='logo' src='/icons/app_icon.png' />
-				</div>
-
-				<h1>Comparazione offerte siti legali in Italia:</h1>
-
-				{bonuses &&
-					bonuses.length > 2 &&
-					bonuses.map((bonus: StreamerBonus) => (
-						<BonusStripe
-							key={`${bonus.name}`}
-							bonus={bonus}
-							countryCode={country}
-						/>
-					))}
-
-				{bonuses &&
-					bonuses.length <= 2 &&
-					streamerData.bonuses.map((bonus: StreamerBonus) => (
-						<BonusStripe
-							key={`${bonus.name}`}
-							bonus={bonus}
-							countryCode={country}
-						/>
-					))}
-
-				<div style={{ padding: '1rem' }}>
-					<VideoDiscalimer />
-				</div>
-				{process.env.REFER === 'true' && (
-					<div className='bottom'>
-						<p style={{ textAlign: 'center' }}>
-							This service is provided by{' '}
-							<a href='https://www.topaffiliation.com'>
-								Top Affiliation
-							</a>
-						</p>
-					</div>
-				)}
-			</Container>
-		</Wrapper>
-	)
-}
+          <div style={{ display: "flex" }}>
+            <img style={{ margin: "auto auto" }} src="/cobra_team.jpg" />
+          </div>
+          <a
+            style={{
+              margin: "2rem",
+              textAlign: "center",
+              display: "block",
+            }}
+            href="https://www.instagram.com/ilcobratipster/"
+          >
+            Segui IL COBRA su instagram cliccando qui!
+          </a>
+        </div>
+      </Container>
+    </Wrapper>
+  );
+};
 
 export async function getServerSideProps({ query }) {
-	const pickedBonus = query.bonuses
-
-	const aquaClient = new AquaClient()
-
-	const bonusToShow = pickedBonus.split('-').map(b => parseInt(b))
-
-	const streamer = await axios.get(
-		`${configuration.api}/streamers/${configuration.streamerId}`
-	)
-
-	return {
-		props: {
-			streamerData: streamer.data as Streamer,
-			bonusToShow: bonusToShow,
-		},
-	}
+  return {
+    props: {},
+  };
 }
 
-export default Compare
+export default index;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,170 +1,56 @@
-import React, { FunctionComponent, useEffect, useState } from 'react'
-import axios from 'axios'
-import { configuration } from '../configuration'
-import AquaClient from '../graphql/aquaClient'
-import { Streamer, StreamerBonus } from '../models/streamer'
-import Wrapper from '../components/Layouts/Wrapper'
-import BonusStripe from '../components/BonusStripe/BonusStripe'
-import VideoDiscalimer from '../components/VideoDisclaimer/VideoDisclaimer'
-import FullPageLoader from '../components/FullPageLoader'
-import Container from '../components/Layouts/Container'
-import lowerCase from 'lodash/lowerCase'
+import React, { FunctionComponent, useEffect, useState } from "react";
+import axios from "axios";
+import { configuration } from "../configuration";
+import AquaClient from "../graphql/aquaClient";
+import { Streamer, StreamerBonus } from "../models/streamer";
+import Wrapper from "../components/Layouts/Wrapper";
+import BonusStripe from "../components/BonusStripe/BonusStripe";
+import VideoDiscalimer from "../components/VideoDisclaimer/VideoDisclaimer";
+import FullPageLoader from "../components/FullPageLoader";
+import Container from "../components/Layouts/Container";
+import lowerCase from "lodash/lowerCase";
 
 interface Props {
-	streamerData: Streamer
+  streamerData: Streamer;
 }
 
 const index: FunctionComponent<Props> = ({ streamerData }) => {
-	const [loading, setLoading] = useState(true)
-	const [country, setCountry] = useState<string>('')
-	useEffect(() => {
-		if (country === 'it') getBonusList()
-		else setLoading(false)
-	}, [country])
-	const [bonuses, setBonuses] = useState<StreamerBonus[] | undefined>(
-		undefined
-	)
-	useEffect(() => {
-		console.log(bonuses)
-	}, [bonuses])
+  return (
+    <Wrapper>
+      <Container>
+        <div className="top-bar">
+          <img className="logo" src="/icons/app_icon.png" />
+        </div>
+        <div>
+          <p style={{ margin: "3rem", marginTop: "4rem" }}>
+            Nel 2015 Paolo e Mimmo hanno creato IL COBRA per condividere con una
+            community crescente la loro passione principale: il calcio e lo
+            sport in generale. Poi è arrivato Anso...
+          </p>
 
-	console.log(streamerData)
-
-	useEffect(() => {
-		geoLocate()
-	}, [])
-
-	const geoLocate = async () => {
-		const userCountryRequest = await axios.get(configuration.geoApi)
-		const countryCode = lowerCase(userCountryRequest.data.country_code2)
-		if (countryCode) setCountry(countryCode)
-	}
-
-	const getBonusList = async () => {
-		let bonusForCountry = streamerData.countryBonusList.filter(
-			it => it.label === country
-		)[0].bonuses
-
-		console.log(bonusForCountry, country)
-
-		if (bonusForCountry.length == 0) {
-			bonusForCountry = streamerData.countryBonusList.filter(
-				it => it.label === 'row'
-			)[0].bonuses
-			setCountry('row')
-		}
-
-		const ordering = streamerData.countryBonusList
-			.filter(it => it.label === country)[0]
-			//@ts-ignore
-			.ordering.split(' ')
-
-		let unorderedBonuses = [...bonusForCountry]
-
-		let ordered: StreamerBonus[] = []
-
-		ordering.forEach(code => {
-			const matchingBonus = unorderedBonuses.find(
-				it => it.compareCode === code
-			)
-			if (matchingBonus) {
-				ordered.push(matchingBonus)
-				unorderedBonuses = unorderedBonuses.filter(
-					b => b.compareCode !== code
-				)
-			}
-		})
-		console.log(streamerData.bonuses)
-		const finalList = [...ordered, ...unorderedBonuses]
-			.map(b => {
-				const match = streamerData.bonuses.find(it => it.id == b.id)
-				if (match !== undefined) return match
-				else return undefined
-			})
-			.filter(it => it !== undefined)
-
-		console.log(finalList, 'final list')
-
-		//@ts-ignore
-		setBonuses(finalList)
-		setLoading(false)
-	}
-
-	if (loading) return <FullPageLoader />
-	return (
-		<Wrapper>
-			<Container>
-				<div className='top-bar'>
-					<img className='logo' src='/icons/app_icon.png' />
-				</div>
-
-				{country === 'it' && (
-					<h1>Comparazione offerte siti legali in Italia:</h1>
-				)}
-
-				{country !== 'it' && (
-					<iframe
-						style={{ margin: '0 auto', display: 'block' }}
-						width='100%'
-						height='500px'
-						src='https://www.youtube.com/embed/z2AbJd3Zxe4'
-					/>
-				)}
-
-				{bonuses &&
-					country === 'it' &&
-					bonuses.length > 2 &&
-					bonuses.map((bonus: StreamerBonus) => (
-						<BonusStripe
-							key={`${bonus.name}`}
-							bonus={bonus}
-							countryCode={country}
-						/>
-					))}
-
-				{bonuses &&
-					country === 'it' &&
-					bonuses.length <= 2 &&
-					streamerData.bonuses.map((bonus: StreamerBonus) => (
-						<BonusStripe
-							key={`${bonus.name}`}
-							bonus={bonus}
-							countryCode={country}
-						/>
-					))}
-
-				<div style={{ padding: '1rem' }}>
-					<VideoDiscalimer />
-				</div>
-				{process.env.REFER === 'true' && (
-					<div className='bottom'>
-						<p style={{ textAlign: 'center' }}>
-							This service is provided by{' '}
-							<a href='https://www.topaffiliation.com'>
-								Top Affiliation
-							</a>
-						</p>
-					</div>
-				)}
-			</Container>
-		</Wrapper>
-	)
-}
+          <div style={{ display: "flex" }}>
+            <img style={{ margin: "auto auto" }} src="/cobra_team.jpg" />
+          </div>
+          <a
+            style={{
+              margin: "2rem",
+              textAlign: "center",
+              display: "block",
+            }}
+            href="https://www.instagram.com/ilcobratipster/"
+          >
+            Segui IL COBRA su instagram cliccando qui!
+          </a>
+        </div>
+      </Container>
+    </Wrapper>
+  );
+};
 
 export async function getServerSideProps({ query }) {
-	const pickedBonus = query.options
-
-	const aquaClient = new AquaClient()
-
-	const streamer = await axios.get(
-		`${configuration.api}/streamers/${configuration.streamerId}`
-	)
-
-	return {
-		props: {
-			streamerData: streamer.data as Streamer,
-		},
-	}
+  return {
+    props: {},
+  };
 }
 
-export default index
+export default index;
